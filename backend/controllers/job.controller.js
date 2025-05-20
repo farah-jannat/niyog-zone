0206e4c8a0f356b777197371b6f4679d66972b97
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-21 00:36:49 +0600

    feat: fetch all jobs

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -52,3 +52,33 @@ export const postJob = async (req, res) => {
     console.log(error);
   }
 };
+
+
+// for students or applicants
+
+export const getAllJobs = async (req, res) => {
+    try {
+        const keyword = req.query.keyword || "";
+        const query = {
+            $or: [
+                { title: { $regex: keyword, $options: "i" } },
+                { description: { $regex: keyword, $options: "i" } },
+            ]
+        };
+        const jobs = await Job.find(query).populate({
+            path: "company"
+        }).sort({ createdAt: -1 });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
