c0123e7e1cec20d09543fa34c10159dc7400b684
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-20 16:13:26 +0600

    feat: get all ocmpanies

--- backend/controllers/company.controller.js
+++ backend/controllers/company.controller.js
@@ -30,3 +30,22 @@ export const registerCompany = async (req, res) => {
     console.log("error while registering company");
   }
 };
+
+export const getCompany = async (req, res) => {
+  try {
+    const userId = req.id;
+    const companies = await Company.find({ userId });
+    if (!companies) {
+      return res.status(404).json({
+        message: "Companies not found.",
+        success: false,
+      });
+    }
+    return res.status(200).json({
+      companies,
+      success: true,
+    });
+  } catch (err) {
+    console.log("eroor while fetching companies", err);
+  }
+};
