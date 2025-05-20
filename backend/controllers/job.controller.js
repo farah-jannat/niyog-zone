beeaca467bcb1eb7e374b87fe2ca51c40cf6591c
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-21 01:14:43 +0600

    feat: show jobs admin created

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -100,3 +100,28 @@ export const getJobById = async (req, res) => {
     console.log(error);
   }
 };
+
+
+
+// how many jobs admin created till now
+export const getAdminJobs = async (req, res) => {
+    try {
+        const adminId = req.id;
+        const jobs = await Job.find({ created_by: adminId }).populate({
+            path:'company',
+            createdAt:-1
+        });
+        if (!jobs) {
+            return res.status(404).json({
+                message: "Jobs not found.",
+                success: false
+            })
+        };
+        return res.status(200).json({
+            jobs,
+            success: true
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
