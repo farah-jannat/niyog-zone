c75593d0f36e46e486521202a5b2da450e9709f2
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-21 15:55:33 +0600

    feat: add get-applicants api

--- backend/controllers/application.controller.js
+++ backend/controllers/application.controller.js
@@ -76,3 +76,31 @@ export const getAppliedJobs = async (req, res) => {
     console.log("error while getting applied jobs", err);
   }
 };
+
+
+// admin gonna check applicants..
+
+export const getApplicants = async (req,res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId).populate({
+            path:'applications',
+            options:{sort:{createdAt:-1}},
+            populate:{
+                path:'applicant'
+            }
+        });
+        if(!job){
+            return res.status(404).json({
+                message:'Job not found.',
+                success:false
+            })
+        };
+        return res.status(200).json({
+            job, 
+            succees:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
