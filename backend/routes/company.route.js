896a9c400e771e4837b98d17c310241f1f84827b
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-24 18:30:27 +0600

    feat: add singleRoute

--- backend/routes/company.route.js
+++ backend/routes/company.route.js
@@ -1,5 +1,7 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
+import { singleUpload } from "../middlewares/multer.js";
+
 import {
   registerCompany,
   getCompany,
@@ -12,6 +14,6 @@ const router = express.Router();
 router.route("/register").post(isAuthenticated, registerCompany);
 router.route("/get").get(isAuthenticated, getCompany);
 router.route("/get/:id").get(isAuthenticated, getCompanyById);
-router.route("/update/:id").put(isAuthenticated, updateCompany);
+router.route("/update/:id").put(isAuthenticated, singleUpload, updateCompany);
 
 export default router;
