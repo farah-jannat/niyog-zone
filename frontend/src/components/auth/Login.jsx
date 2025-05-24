c051016d893233de64245c2998a96673bff6b00e
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-24 19:37:48 +0600

    feat: implement loading functionality

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -8,6 +8,10 @@ import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../shared/Navbar";
 import { USER_API_END_POINT } from "@/utils/constant";
 import { toast } from "sonner";
+import { useDispatch, useSelector } from "react-redux";
+import { setLoading } from "@/redux/authSlice";
+import store from "@/redux/store";
+import { Loader2 } from "lucide-react";
 
 const Login = () => {
   const [input, setInput] = useState({
@@ -15,7 +19,9 @@ const Login = () => {
     password: "",
     role: "",
   });
+  const { loading } = useSelector((store) => store.auth);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const changeEvenHandler = (e) => {
     setInput({ ...input, [e.target.name]: e.target.value });
   };
@@ -23,6 +29,7 @@ const Login = () => {
   const submitHandler = async (e) => {
     e.preventDefault();
     try {
+      dispatch(setLoading(true));
       const res = await axios.post(`${USER_API_END_POINT}/login`, input, {
         headers: {
           "Content-Type": "application/json",
@@ -36,6 +43,8 @@ const Login = () => {
       }
     } catch (err) {
       console.log("eroor form login page", err);
+    } finally {
+      dispatch(setLoading(false));
     }
   };
 
@@ -99,9 +108,17 @@ const Login = () => {
               </div>
             </RadioGroup>
           </div>
-          <Button type="submit" className="w-full my-4">
-            Login
-          </Button>
+          {loading ? (
+            <Button className="w-full my-4">
+              <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              please wait
+            </Button>
+          ) : (
+            <Button type="submit" className="w-full my-4">
+              Login
+            </Button>
+          )}
+
           <span className="text-sm">
             Don't have an account?
             <Link to="/signup" className="text-blue-600">
