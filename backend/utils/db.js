2a3c44cf50dcbc50e5f9c6ff5d5b28b876ccfa66
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-24 18:31:10 +0600

    feat: update mongoose filed

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -1,11 +1,20 @@
 import mongoose from "mongoose";
+import { User } from "../models/user.model.js";
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("mongodb connected succesfully");
-  } catch (err) {
-    console.log(err);
+    const result = await User.updateMany(
+      { fullname: { $exists: true } }, // Query: Find documents where 'fullname' field exists
+      { $rename: { fullname: "fullName" } } // Update: Rename 'fullname' to 'fullName'
+    );
+
+    console.log(
+      `Migration complete: ${result.matchedCount} documents matched, ${result.modifiedCount} documents modified.`
+    );
+  } catch (error) {
+    console.error("Migration failed:", error);
   }
 };
 
