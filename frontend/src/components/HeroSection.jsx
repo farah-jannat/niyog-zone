b7b5ce6fb81b2952751e1f50932223a626c7478a
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-25 17:21:44 +0600

    feat: add hero-section

---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+import { Search } from "lucide-react";
+
+const HeroSection = () => {
+  return (
+    <div className="text-center">
+      <div className="flex flex-col gap-5 my-10 mx-auto ">
+        <span className="bg-gray-100 text-[#F83002] px-4 mx-auto rounded-full py-2 font-medium">
+          No. 1 Job Hunt Website
+        </span>
+        <h1 className="text-5xl font-bold">
+          Search, Apply & <br />
+          Get Your
+          <span className="text-[#6A38C2]"> Dream Jobs</span>
+        </h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+          distinctio voluptate! Enim, sint culpa!
+        </p>
+        <div className=" border border-gray-200 rounded-full pl-3 flex items-center gap-4 shadow-lg  mx-auto w-[40%]">
+          <Input
+            type="text"
+            placeholder="Find your dream Jobs"
+            className="outline-none border-none w-full"
+          />
+          <Button className="rounded-r-full bg-[#6A38C2]">
+            <Search className="h-5 w-5" />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
