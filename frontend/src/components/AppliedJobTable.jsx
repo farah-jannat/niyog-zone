582d7a192ec8071ccf3fd01ce3f41ccde468db0e
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-27 16:20:31 +0600

    feat: add application-table

---
+++ frontend/src/components/AppliedJobTable.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import { Badge } from "./ui/badge";
+
+const AppliedJobTable = () => {
+  return (
+    <div>
+      <Table>
+        <TableCaption>A list of your applied jobs</TableCaption>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Data</TableHead>
+            <TableHead>Job role</TableHead>
+            <TableHead>company</TableHead>
+            <TableHead className="text-right">Status</TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {[1, 2, 3, 4, 5, 6].map((item, index) => (
+            <TableRow key={index}>
+              <TableCell>17-07-2024</TableCell>
+              <TableCell>Frontend Developer</TableCell>
+              <TableCell>Google</TableCell>
+              <TableCell className="text-right">
+                <Badge>Selected</Badge>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+};
+
+export default AppliedJobTable;
