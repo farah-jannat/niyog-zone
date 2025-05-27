dfc432d76ebf972ee69f590a699faf05d8f8aed5
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-27 09:40:38 +0600

    feat: add latest-job-cards

---
+++ frontend/src/components/LatestJobsCards.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { Badge } from "./ui/badge";
+
+const LatestJobsCards = () => {
+  return (
+    <div className="bg-white p-5 rounded-md border border-gray-100 shadow-xl cursor-pointer">
+      <div>
+        <h1 className="font-medium text-lg">Company Name</h1>
+        <p className="text-sm text-gray-500">Bangladesh</p>
+      </div>
+      <div>
+        <h1 className="font-bold text-lg my-2">Job Title</h1>
+        <p className="text-sm text-gray-600">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur.
+        </p>
+      </div>
+      <div className="flex items-center gap-2 mt-4">
+        <Badge className={"text-blue-700 font-bold"} variant="ghost">
+          12 positions
+        </Badge>
+        <Badge className={"text-[#F83002] font-bold"} variant="ghost">
+          {" "}
+          part time
+        </Badge>
+        <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
+          24 LPA
+        </Badge>
+      </div>
+    </div>
+  );
+};
+
+export default LatestJobsCards;
