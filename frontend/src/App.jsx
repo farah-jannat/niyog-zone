66122ca0de8d2b7f6c34265a7ab250f7cf24304b
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-27 16:20:52 +0600

    feat: add required routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,15 @@ import Navbar from "./components/shared/Navbar";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
 import Home from "./components/Home";
+import Jobs from "./components/Jobs";
+import Browse from "./components/Browse";
+import Profile from "./components/Profile";
+import JobDescription from "./components/JobDescription";
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
-    element: <Home  />,
+    element: <Home />,
   },
   {
     path: "/login",
@@ -17,13 +21,28 @@ const appRouter = createBrowserRouter([
     path: "/signup",
     element: <Signup />,
   },
+  {
+    path: "/jobs",
+    element: <Jobs />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
+  {
+    path: "/description/:id",
+    element: <JobDescription />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
 ]);
 
 function App() {
   return (
     <>
       <RouterProvider router={appRouter} />
-
     </>
   );
 }
