072f54c28c4bd947581acd1c1175c8a268d11366
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-27 08:50:56 +0600

    feat: add home page

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "./shared/Navbar";
+import HeroSection from "./HeroSection";
+import CategoryCarousel from "./categoryCarousel";
+import LatestJobs from "./LatestJobs";
+import Footer from "./shared/Footer";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <HeroSection />
+      <CategoryCarousel />
+      <LatestJobs />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
