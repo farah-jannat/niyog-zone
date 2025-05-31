eaf6bef7892748ff0a16c74d11a955a6b4affe5f
“farah-jannat” <farahjannatmithila@gmail.com>
2025-05-31 13:51:26 +0600

    refactor: replace hardcoded data to dynamic

--- frontend/src/components/LatestJobsCards.jsx
+++ frontend/src/components/LatestJobsCards.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import { Badge } from "./ui/badge";
 
-const LatestJobsCards = () => {
+const LatestJobsCards = ({ job }) => {
   return (
     <div className="bg-white p-5 rounded-md border border-gray-100 shadow-xl cursor-pointer">
       <div>
-        <h1 className="font-medium text-lg">Company Name</h1>
+        <h1 className="font-medium text-lg">{job?.company?.name}</h1>
         <p className="text-sm text-gray-500">Bangladesh</p>
       </div>
       <div>
-        <h1 className="font-bold text-lg my-2">Job Title</h1>
-        <p className="text-sm text-gray-600">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur.
-        </p>
+        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
+        <p className="text-sm text-gray-600">{job?.description}</p>
       </div>
       <div className="flex items-center gap-2 mt-4">
         <Badge className={"text-blue-700 font-bold"} variant="ghost">
-          12 positions
+          {job?.position}
         </Badge>
         <Badge className={"text-[#F83002] font-bold"} variant="ghost">
           {" "}
-          part time
+          {job?.jobType}
         </Badge>
         <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
-          24 LPA
+          {job?.salary}
         </Badge>
       </div>
     </div>
