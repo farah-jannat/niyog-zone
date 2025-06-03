e67c586dbbe5dd70db4a6d571e60555d533ae583
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-03 13:00:32 +0600

    feat: add appication-table functionality

--- frontend/src/components/admin/ApplicantsTable.jsx
+++ frontend/src/components/admin/ApplicantsTable.jsx
@@ -10,9 +10,32 @@ import {
 } from "../ui/table";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { MoreHorizontal } from "lucide-react";
+import { useSelector } from "react-redux";
+import { toast } from "sonner";
+import { APPLICATION_API_END_POINT } from "@/utils/constant";
+import axios from "axios";
 
 const shortlisingStatus = ["Accepted", "Rejected"];
 const ApplicantsTable = () => {
+  const { applicants } = useSelector((store) => store.application);
+
+  const statusHandler = async (status, id) => {
+    console.log("called");
+    try {
+      axios.defaults.withCredentials = true;
+      const res = await axios.post(
+        `${APPLICATION_API_END_POINT}/status/${id}/update`,
+        { status }
+      );
+      console.log(res);
+      if (res.data.success) {
+        toast.success(res.data.message);
+      }
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  };
+
   return (
     <div>
       <Table>
@@ -28,29 +51,49 @@ const ApplicantsTable = () => {
           </TableRow>
         </TableHeader>
         <TableBody>
-          <tr>
-            <TableCell>Full Name</TableCell>
-            <TableCell>Email</TableCell>
-            <TableCell>Contact</TableCell>
-            <TableCell>Resume</TableCell>
-            <TableCell>Date</TableCell>
-            <TableCell className="text-right">
-              <Popover>
-                <PopoverTrigger>
-                  <MoreHorizontal />
-                  <PopoverContent className="w-32">
-                    {shortlisingStatus.map((status, index) => {
-                      return (
-                        <div key={index}>
-                          <span>{status}</span>
-                        </div>
-                      );
-                    })}
-                  </PopoverContent>
-                </PopoverTrigger>
-              </Popover>
-            </TableCell>
-          </tr>
+          {applicants &&
+            applicants?.applications?.map((item) => (
+              <tr>
+                <TableCell>{item?.applicant?.fullName}</TableCell>
+                <TableCell>{item?.applicant?.email}</TableCell>
+                <TableCell>{item?.applicant?.phoneNumber}</TableCell>
+                <TableCell>
+                  {item.applicant?.profile?.resume ? (
+                    <a
+                      className="text-blue-600 cursor-pointer"
+                      href={item?.applicant?.profile?.resume}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {item?.applicant?.profile?.resumeOriginalName}
+                    </a>
+                  ) : (
+                    <span>NA</span>
+                  )}
+                </TableCell>
+                <TableCell>{item?.applicant.createdAt.split("T")[0]}</TableCell>
+                <TableCell className="float-right  cursor-pointer">
+                  <Popover>
+                    <PopoverTrigger>
+                      <MoreHorizontal />
+                      <PopoverContent className="w-32">
+                        {shortlisingStatus.map((status, index) => {
+                          return (
+                            <div
+                              onClick={() => statusHandler(status, item?._id)}
+                              key={index}
+                              className="flex w-fit items-center my-2 cursor-pointer"
+                            >
+                              <span>{status}</span>
+                            </div>
+                          );
+                        })}
+                      </PopoverContent>
+                    </PopoverTrigger>
+                  </Popover>
+                </TableCell>
+              </tr>
+            ))}
         </TableBody>
       </Table>
     </div>
