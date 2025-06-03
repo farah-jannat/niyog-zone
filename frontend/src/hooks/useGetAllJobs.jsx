ba6b467b19cb232420cc94715272bebf0eef66b8
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-03 12:46:17 +0600

    feat: update get-all-jobs hook

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -6,13 +6,14 @@ import { useDispatch, useSelector } from "react-redux";
 
 const useGetAllJobs = () => {
   const dispatch = useDispatch();
-  
+  const { searchedQuery } = useSelector((store) => store.job);
   useEffect(() => {
     const fetchAllJobs = async () => {
       try {
-        const res = await axios.get(`${JOB_API_END_POINT}/get`, {
-          withCredentials: true,
-        });
+        const res = await axios.get(
+          `${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,
+          { withCredentials: true }
+        );
         if (res.data.success) {
           dispatch(setAllJobs(res.data.jobs));
         }
