1f3fc63839c5e03314b43d4bab33f18a928941d2
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-03 13:02:42 +0600

    feat: Prevent reaccess to sign-up page after authentication

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
@@ -23,7 +23,7 @@ const Signup = () => {
     file: "",
   });
   const navigate = useNavigate();
-  const { loading } = useSelector((store) => store.auth);
+  const { loading, user } = useSelector((store) => store.auth);
   const dispatch = useDispatch();
 
   const changeEvenHandler = (e) => {
@@ -67,6 +67,11 @@ const Signup = () => {
       dispatch(setLoading(false));
     }
   };
+  useEffect(() => {
+    if (user) {
+      navigate("/");
+    }
+  }, []);
   return (
     <div>
       <Navbar />
