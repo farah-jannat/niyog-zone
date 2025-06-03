ba6c700368e0950b2ad6228ece00c21eced08ac3
“farah-jannat” <farahjannatmithila@gmail.com>
2025-06-03 12:54:22 +0600

    feat: add index.js

--- backend/index.js
+++ backend/index.js
@@ -9,20 +9,22 @@ import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
 dotenv.config({});
+
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-  origin: "http//localhost:5173",
-  credentials: true,
-};
+    origin:'http://localhost:5173',
+    credentials:true
+}
 
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
 
 // api's
 app.use("/api/v1/user", userRoute);
@@ -30,7 +32,9 @@ app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`server is running at port${PORT}`);
-});
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
