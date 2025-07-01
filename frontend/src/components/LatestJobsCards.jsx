27e55cf48069d0b675af5fb0af8bac25974a41df
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-01 23:01:00 +0600

    style: update jobscard design

--- frontend/src/components/LatestJobsCards.jsx
+++ frontend/src/components/LatestJobsCards.jsx
@@ -1,33 +1,71 @@
 import React from "react";
 import { Badge } from "./ui/badge";
 import { useNavigate } from "react-router-dom";
+import { BriefcaseBusiness, Clock7, Locate, MapPin } from "lucide-react";
+import { Button } from "./ui/button";
 
 const LatestJobsCards = ({ job }) => {
   const navigate = useNavigate();
   return (
     <div
       onClick={() => navigate(`/description/${job._id}`)}
-      className="bg-white p-5 rounded-md border border-gray-100 shadow-xl cursor-pointer"
+      className="flex flex-col gap-5 bg-white p-5 rounded-md border border-gray-100 shadow-xl cursor-pointer"
     >
-      <div>
-        <h1 className="font-medium text-lg">{job?.company?.name}</h1>
-        <p className="text-sm text-gray-500">Bangladesh</p>
+      <div className="flex items-center gap-3">
+        <img src={job?.company?.logo} alt="" className="rounded-md h-12 w-12" />
+
+        <div>
+          <h1 className="capitalize font-semibold">{job?.company?.name}</h1>
+          <div className="text-sm text-gray-500 flex items-center gap-1">
+            <MapPin size={15} />
+            Bangladesh
+          </div>
+        </div>
       </div>
+
       <div>
-        <h1 className="font-bold text-lg my-2">{job?.title}</h1>
-        <p className="text-sm text-gray-600">{job?.description}</p>
+        <h1 className="font-semibold capitalize ">{job?.title}</h1>
+        <div className="flex gap-3">
+          <div className="flex gap-1 items-center text-sm text-gray-500 mr-3">
+            <BriefcaseBusiness size={15} />
+            Fulltime
+          </div>
+          <div className="flex gap-1 items-center text-sm text-gray-500">
+            <Clock7 size={15} />3 days ago
+          </div>
+        </div>
       </div>
-      <div className="flex items-center gap-2 mt-4">
-        <Badge className={"text-blue-700 font-bold"} variant="ghost">
+
+      <p className="text-sm text-gray-700">{job?.description}</p>
+      <div className="flex items-center gap-2 ">
+        <Badge
+          className={
+            "text-gray-500 rounded-md border-none font-bold bg-gray-100 py-2 px-3 "
+          }
+          variant="outline"
+        >
           {job?.position}
         </Badge>
-        <Badge className={"text-[#F83002] font-bold"} variant="ghost">
+        <Badge
+          className={
+            " text-gray-500 rounded-md border-none font-bold bg-gray-100 py-2 px-3"
+          }
+          variant="outline"
+        >
           {" "}
           {job?.jobType}
         </Badge>
-        <Badge className={"text-[#7209b7] font-bold"} variant="ghost">
-          {job?.salary}
+      </div>
+      <div className="flex justify-between">
+        <Badge
+          className={"text-blue-700 font-bold capitalize p-0 border-none"}
+          variant="ghost"
+        >
+          ${job?.salary}/hour
         </Badge>
+        <button className="rounded-md px-3 py-2 text-sm font-semibold text-blue-900 bg-blue-100">
+          Apply now
+        </button>
       </div>
     </div>
   );
