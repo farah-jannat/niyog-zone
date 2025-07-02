a365a35d62513dc01d64b8505aadb25c372f383e
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-02 22:38:01 +0600

    style: change hero-section css

--- frontend/src/components/HeroSection.jsx
+++ frontend/src/components/HeroSection.jsx
@@ -17,21 +17,18 @@ const HeroSection = () => {
   };
 
   return (
-    <div className="text-center w-[70%] mx-auto md:w-[90%]">
+    <div className="text-center w-[70%] mx-auto md:w-[80%] text-white">
       <div className="flex flex-col gap-5 my-10 ">
-        <span className="hidden md:block bg-gray-100 text-blue-900 px-4 mx-auto rounded-full py-2 font-medium">
-          No. 1 Job Hunt Website
-        </span>
-        <h1 className=" text-3xl md:text-5xl font-bold">
-          Search, Apply & <br />
-          Get Your
-          <span className="text-blue-900"> Dream Jobs</span>
+        <h1 className=" text-3xl md:text-4xl font-bold">
+          The #1 Job Board for Hiring <br />
+          or Find Your next Job
         </h1>
-        <p className="text-gray-500">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
-          distinctio voluptate! Enim, sint culpa!
+        <p className="text-text_white">
+          Each month more than 3 million job seekers turn to website in their
+          search for <br /> work, making over 140,000 applications every single
+          day
         </p>
-        <div className=" border border-gray-200 text-gray-500 rounded-xl py-2 pl-5 flex items-center gap-3 shadow-lg  mx-auto w-[full] ">
+        <div className="bg-White border border-gray-200 text-gray-500 rounded-md py-2 pl-5 flex items-center gap-3 shadow-lg  mx-auto w-[full] ">
           <div className="flex flex-1 items-center gap-2 ">
             <Search size={20} />
             <Input
@@ -53,25 +50,24 @@ const HeroSection = () => {
             <span>location</span>
             <ChevronDown size={17} />
           </div>
-          <Button
-            onClick={searchJobHandler}
-            className="rounded-r-xl h-auto bg-button_blue"
-          >
+          <Button onClick={searchJobHandler} className="rounded-md bg-Blue">
             search
           </Button>
         </div>
-        <p className="text-gray-500 ">
-          popular searches:{" "}
-          <a className="hover:underline" href="#">
-            Designer
-          </a>{" "}
-          <a className="hover:underline" href="#">
-            Web{" "}
+        <p className="hidden sm:block text-text_white ">
+          popular searches:{"   "}
+          <a className="underline" href="#">
+            Designer,
           </a>
-          <a className="hover:underline" href="">
-            IOS Developer
-          </a>{" "}
-          <a className="hover:underline" href="">
+          {"  "}
+          <a className="underline" href="#">
+            Web,{"  "}
+          </a>
+          <a className="underline" href="">
+            IOS Developer,
+          </a>
+          {"  "}
+          <a className="underline" href="">
             Php
           </a>
         </p>
