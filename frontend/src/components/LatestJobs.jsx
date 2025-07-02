39787e1da63e54b484977154018b84b5b3e33feb
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-02 22:39:08 +0600

    style: update job-card design

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -1,17 +1,36 @@
 import React from "react";
 import LatestJobsCards from "./LatestJobsCards";
 import { useSelector } from "react-redux";
+import { Button } from "./ui/button";
+import { Home } from "lucide-react";
 
 const LatestJobs = () => {
   const { allJobs } = useSelector((store) => store.job);
   console.log("all job here", allJobs);
   return (
-    <div className="max-w-7xl mx-auto my-20">
-      <h1 className="text-4xl font-bold">
-        {" "}
-        <span className="text-[#6A38C2]">Latest & Top</span>Job Openings
-      </h1>
-      <div className="grid grid-cols-3 gap-4 my-5">
+    <div className="w-[90%] lg:w-[80%] mx-auto mt-10">
+      <div className="flex flex-col items-center gap-4">
+        <h2 className="text-2xl font-semibold text-center">Latest Jobs Post</h2>
+        <p className="text-gray-500 text-center">
+          Find the job thats prfect for you, about 800+ new jobs everyday
+        </p>
+        <div className="flex items-center justify-center flex-wrap gap-3 sm:gap-4">
+          <div className=" flex items-center gap-2 border border-Blue rounded-md px-3 py-2">
+            <Home size={20} className="text-Blue" />
+            <button className="text-sm font-semibold text-Blue ">
+              Marketing
+            </button>
+          </div>
+          {[1, 2, 3, 4, 5].map((item, idx) => (
+            <div className="flex items-center gap-2 border border-gray-150 rounded-md px-3 py-2">
+              <Home size={20} className="text-blue-900" />
+              <button className="text-sm font-semibold  ">Marketing</button>
+            </div>
+          ))}
+        </div>
+      </div>
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-5"> */}
+      <div className="flex items-center justify-center  gap-3 flex-wrap ">
         {allJobs.length <= 0 ? (
           <span>No Job Available</span>
         ) : (
