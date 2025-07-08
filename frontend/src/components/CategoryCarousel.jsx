4d095bcf3694e7061ee142173f78eb71353fdf4e
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-08 22:17:49 +0600

    refactor: add swiper carousel

--- frontend/src/components/CategoryCarousel.jsx
+++ frontend/src/components/CategoryCarousel.jsx
@@ -11,6 +11,16 @@ import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { setSearchedQuery } from "@/redux/jobSlice";
 import { BookOpenText, Home } from "lucide-react";
+// import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+import { FreeMode, Pagination } from "swiper/modules";
 
 const category = [
   {
@@ -67,53 +77,65 @@ const category = [
 const CategoryCarousel = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const searchJobHandler = (query) => {
-    dispatch(setSearchedQuery(query));
-    navigate("/browse");
-  };
+  // const searchJobHandler = (query) => {
+  //   dispatch(setSearchedQuery(query));
+  //   navigate("/browse");
+  // };
 
   return (
-    <div>
-      <div className="text-center mt-20 ">
-        {/* <h2 className="text-2xl font-semibold">Browse Jobs Category</h2>
-        <h1 className="text-xs text-gray-500 mt-2">
-          Find the Jobs thats perfect for you, about 800+ new jobs everyday
-        </h1> */}
-      </div>
-      <div className="mx-auto flex flex-col items-center gap-5   w-[70%] md:w-[80%] my-10">
-        <Carousel className="w-full">
-          <CarouselContent >
-            {category.map((cat, index) => (
-              <CarouselItem className="hidden  sm:block basis-1/2 md:basis-1/3 lg:basis-1/4">
-                <div
-                  onClick={() => searchJobHandler(cat)}
-                  className="flex items-center rounded-md p-3  gap-3 border shadow-md bg-White border-gray-200 "
-                >
-                  <BookOpenText
-                    size={40}
-                    strokeWidth="1"
-                    className="text-blue-900"
-                  />
-                  <div className="flex flex-col">
-                    <p className="font-semibold text-[14px]"> {cat.name}</p>
-                    <span className="text-xs text-gray-500">{cat.desc}</span>
-                  </div>
+    <>
+      <div className="mx-auto flex flex-col items-center gap-5   w-[90%] md:w-[80%] my-10 ">
+        <Swiper
+          breakpoints={{
+            500: {
+              slidesPerView: 2,
+              spaceBetween: 5,
+            },
+            // 700:{
+            //   slidesPerView: 3,
+            //   spaceBetween: 5,
+            // },
+            1000: {
+              slidesPerView: 3,
+              spaceBetween: 3,
+            },
+            1200: {
+              slidesPerView: 4,
+              spaceBetween: 3,
+            },
+          }}
+          slidesPerView={1}
+          spaceBetween={5}
+          freeMode={true}
+          pagination={{
+            clickable: true,
+          }}
+          modules={[FreeMode, Pagination]}
+          className="mySwiper  w-[90%] md:w-[80%]"
+        >
+          {category.map((cat, index) => (
+            <SwiperSlide key={index}>
+              <div
+                onClick={() => {
+                  navigate(`/jobs?search=${cat.name}`);
+                }}
+                className="flex items-center rounded-md p-3 gap-3 border shadow-md bg-white border-gray-200 mb-10"
+              >
+                <BookOpenText
+                  size={40}
+                  strokeWidth="1"
+                  className="text-blue-900"
+                />
+                <div className="flex flex-col">
+                  <p className="font-semibold text-[14px]"> {cat.name}</p>
+                  <span className="text-xs text-gray-500">{cat.desc}</span>
                 </div>
-              </CarouselItem>
-            ))}
-          </CarouselContent>
-          {/* <CarouselPrevious className="bg-blue-100 text-blue-900" />
-        <CarouselNext className="bg-blue-100 text-blue-900" /> */}
-        </Carousel>
-        <div className="hidden sm:flex items-center gap-2">
-          <div className="h-[6px] w-[6px] bg-blue-200 rounded-full"></div>
-          <div className="h-[10px] w-[10px] bg-White rounded-full"></div>
-          <div className="h-[6px] w-[6px] bg-blue-200 rounded-full"></div>
-          <div className="h-[6px] w-[6px] bg-blue-200 rounded-full"></div>
-          <div className="h-[6px] w-[6px] bg-blue-200 rounded-full"></div>
-        </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
-    </div>
+    </>
   );
 };
 
