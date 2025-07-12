91dfc9673b82bb441f425544fd0f37f246e822cb
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-12 21:03:58 +0600

    fix: fix route

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -5,13 +5,15 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 const useGetAllJobs = () => {
+  // console.log("hello")
   const dispatch = useDispatch();
   const { searchedQuery } = useSelector((store) => store.job);
   useEffect(() => {
     const fetchAllJobs = async () => {
       try {
         const res = await axios.get(
-          `${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,
+          // `${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,
+          `${JOB_API_END_POINT}/get`,
           { withCredentials: true }
         );
         if (res.data.success) {
