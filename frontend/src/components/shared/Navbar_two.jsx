555a0840381335a92f84628fb8715bfc94687d6c
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-12 21:02:33 +0600

    feat: add color to navbar-menu

--- frontend/src/components/shared/Navbar_two.jsx
+++ frontend/src/components/shared/Navbar_two.jsx
@@ -3,7 +3,7 @@ import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Bell, LogOut, User2 } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "sonner";
 import axios from "axios";
@@ -11,6 +11,7 @@ import { USER_API_END_POINT } from "@/utils/constant";
 import { setUser } from "@/redux/authSlice";
 
 const Navbar_two = () => {
+  const location = useLocation();
   const { user } = useSelector((store) => store.auth);
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -35,9 +36,11 @@ const Navbar_two = () => {
     <div className="w-[full] shadow-sm">
       <div className="flex justify-between items-center w-[80%] mx-auto h-16 text-Black ">
         <div>
-          <h1 className="text-2xl font-bold text-Black">
-            Niyog<span className="text-Blue">Zone</span>
-          </h1>
+          <Link to="/">
+            <h1 className="text-2xl font-bold text-Black cursor-pointer">
+              Niyog<span className="text-Blue">Zone</span>
+            </h1>
+          </Link>
         </div>
 
         <div>
@@ -45,19 +48,55 @@ const Navbar_two = () => {
             {user && user.role == "recruiter" ? (
               <>
                 <li>
-                  <Link to="/admin/companies">Companies</Link>
+                  <Link
+                    to="/admin/companies"
+                    className={`text-lg font-medium transition-colors duration-200 ${
+                      location.pathname === "/admin/companies"
+                        ? "text-blue-600 " // Active style
+                        : "text-gray-700 hover:text-blue-500" // Inactive style
+                    }`}
+                  >
+                    Companies
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/admin/jobs">Jobs</Link>
+                  <Link
+                    to="/admin/jobs"
+                    className={`text-lg font-medium transition-colors duration-200 ${
+                      location.pathname === "/admin/jobs"
+                        ? "text-blue-600 " // Active style
+                        : "text-gray-700 hover:text-blue-500" // Inactive style
+                    }`}
+                  >
+                    Jobs
+                  </Link>
                 </li>
               </>
             ) : (
               <>
                 <li>
-                  <Link to="/">Home</Link>
+                  <Link
+                    to="/"
+                    className={`text-lg font-medium transition-colors duration-200 ${
+                      location.pathname === "/"
+                        ? "text-blue-600 " // Active style
+                        : "text-gray-700 hover:text-blue-500" // Inactive style
+                    }`}
+                  >
+                    Home
+                  </Link>
                 </li>
                 <li>
-                  <Link to="/jobs">Find a job</Link>
+                  <Link
+                    to="/jobs"
+                    className={`text-lg font-medium transition-colors duration-200 ${
+                      location.pathname === "/jobs"
+                        ? "text-blue-600 " // Active style
+                        : "text-gray-700 hover:text-blue-500" // Inactive style
+                    }`}
+                  >
+                    Find a job
+                  </Link>
                 </li>
                 <li>
                   <Link to="/browse">My Network</Link>
@@ -78,16 +117,16 @@ const Navbar_two = () => {
             </div>
           ) : (
             <div className="flex items-center gap-5">
-              <div className="relative">
+              {/* <div className="relative">
                 <Bell size={28} />
                 <div className="absolute -top-2 -right-2 w-5 h-5 flex items-center justify-center bg-red-500 text-white rounded-full text-xs">
                   1
                 </div>
-              </div>
+              </div> */}
 
               <div className="hidden sm:flex flex-col items-start">
-                <h2 className="font-semibold">Emma W</h2>
-                <h1 className="text-sm text-gray-500">Recruiter</h1>
+                <h2 className="font-semibold">{user?.fullName}</h2>
+                <h1 className="text-sm text-gray-500">{user?.role}</h1>
               </div>
 
               <Popover>
