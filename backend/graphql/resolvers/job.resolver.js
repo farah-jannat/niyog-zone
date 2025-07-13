1056cf1fce5582f2e497276f78f625593eb7dffb
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-13 17:29:52 +0600

    feat: add job resolver

---
+++ backend/graphql/resolvers/job.resolver.js
@@ -0,0 +1,63 @@
+import { Job } from "../../models/job.model.js";
+
+export const jobQueries = {
+  async getAllJobs(_, { keyword }, context) {
+    try {
+      const searchKeyword = keyword || "";
+
+      const query = {
+        $or: [
+          { title: { $regex: searchKeyword, $options: "i" } },
+          { description: { $regex: searchKeyword, $options: "i" } },
+        ],
+      };
+
+      const jobs = await Job.find(query)
+        .populate({
+          path: "company",
+        })
+        .sort({ createdAt: -1 });
+
+      if (!jobs) {
+        return [];
+      }
+
+      return jobs;
+    } catch (error) {
+      console.error("Error in getAllJobs resolver:", error);
+      throw new Error("Failed to fetch jobs.");
+    }
+  },
+
+  async getJobById(_, { jobId }, context) {
+    try {
+      const job = await Job.findById(jobId).populate({
+        path: "applications",
+      });
+
+      if (!job) return {};
+
+      return job;
+    } catch (error) {
+      throw new Error("Failed to fetch job");
+    }
+  },
+
+  async getAdminJobs(_, { adminId }, context) {
+    try {
+      const jobs = await Job.find({ created_by: adminId }).populate({
+        path: "company",
+        createdAt: -1,
+      });
+      if (!jobs) return [];
+
+      return jobs;
+    } catch (error) {
+      console.log(error);
+
+      throw new Error("Failed to fetch data");
+    }
+  },
+};
+
+export const jobMutations = {};
