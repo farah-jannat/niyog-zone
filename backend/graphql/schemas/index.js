26a1e4cfcd2715efda76e7b01addd137c0196808
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-13 17:30:30 +0600

    feat: add schemas

--- backend/graphql/schemas/index.js
+++ backend/graphql/schemas/index.js
@@ -10,8 +10,30 @@ const typeDefs = `#graphql
   ${companySchema}
 
   type Query {
-    getAllJobs: [Job]!
+    getAllJobs(keyword:String): [Job]!
+    getJobById(jobId: ID!): Job
+    getAdminJobs(adminId: ID!): [Job]!
   }
+
+  type Mutation{
+    register(registerInput: RegisterInput): User!
+  }
+
+
+scalar Upload
+
+
+input RegisterInput {
+  fullName: String!
+  email: String!
+  phoneNumber: String!
+  password: String!
+  role: String!
+  profilePhoto: Upload # Use the Upload scalar for files
+  resume: Upload       # Use the Upload scalar for files
+}
+
+
 `;
 
 export default typeDefs;
