4698cb00da006141d5cf0815dd70370fbfca33f8
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-15 23:56:14 +0600

    feat: update index.js graphql schema

--- backend/graphql/schemas/index.js
+++ backend/graphql/schemas/index.js
@@ -13,10 +13,21 @@ const typeDefs = `#graphql
     getAllJobs(keyword:String): [Job]!
     getJobById(jobId: ID!): Job
     getAdminJobs(adminId: ID!): [Job]!
+    getCompanies(userId:ID!):[Company]!
+    getCompanyById(companyId:ID!):Company!
+    getAppliedJobs(userId:ID!):[Application]
+    getApplicants(jobId:ID!):Job
   }
 
   type Mutation{
     register(registerInput: RegisterInput): User!
+    login(loginInput:loginInput):User
+    update(updateInput:updateInput):User
+    postJob(postInput:postInput):Job
+    registerCompany(companyRegisterInput:companyRegisterInput):Company
+    applyJob(applyJobInput:applyJobInput):Application
+    updateStatus(updateStatusInput:updateStatusInput):Application
+    updateCompany(updateCompanyInput:updateCompanyInput):Company
   }
 
 
@@ -31,6 +42,68 @@ input RegisterInput {
   role: String!
   profilePhoto: String
   resume: String
+}
+input loginInput {
+  
+  email: String!
+  password: String!
+  role: String!
+  
+}
+input updateInput {
+  
+  fullName: String
+  userId: ID!
+  email: String
+  phoneNumber: String
+  password: String
+  role: String
+  profilePhoto: String
+  resume: String
+  
+}
+
+input postInput {
+    title: String!
+    userId: String!
+    description: String!
+    requirements: [String]
+    salary: Int!
+    experienceLevel: Int!
+    location: String!
+    jobType: String!
+    position: Int!
+    companyId : String!
+    
+}
+input companyRegisterInput {
+    
+    userId: String!
+    companyName:String!
+    
+}
+input applyJobInput {
+    
+    userId: String!
+    jobId:String!
+    
+}
+input updateStatusInput {
+    
+    status:String!
+    applicationId: String!
+    
+}
+input updateCompanyInput {
+    
+    
+    name:String!
+    description:String
+    website:String
+    location:String
+    logo:String
+    companyId :String
+    
 }
 
 
