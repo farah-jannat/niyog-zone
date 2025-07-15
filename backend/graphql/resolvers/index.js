1de83013158e9d9705839d18627de71050eea204
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-15 23:53:55 +0600

    feat: update resolver index.js

--- backend/graphql/resolvers/index.js
+++ backend/graphql/resolvers/index.js
@@ -1,13 +1,23 @@
-import { jobQueries } from "./job.resolver.js";
+import {
+  applicationMutations,
+  applicationQueries,
+} from "./application.resolver.js";
+import { companyMutations, companyQueries } from "./company.resolver.js";
+import { jobMutations, jobQueries } from "./job.resolver.js";
 import { userMutations } from "./user.resolver.js";
 
 const resolvers = {
   Query: {
     ...jobQueries,
+    ...companyQueries,
+    ...applicationQueries,
   },
-    Mutation: {
-      ...userMutations,
-    },
+  Mutation: {
+    ...userMutations,
+    ...jobMutations,
+    ...companyMutations,
+    ...applicationMutations,
+  },
 };
 
 export default resolvers;
