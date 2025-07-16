6680ccfd015fe645eba5451466428e46d39c77b1
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-16 22:38:22 +0600

    refactor: add grapql query

--- frontend/src/hooks/useGetAllJobs.jsx
+++ frontend/src/hooks/useGetAllJobs.jsx
@@ -3,28 +3,52 @@ import { JOB_API_END_POINT } from "@/utils/constant";
 import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { gql, useQuery } from "@apollo/client";
 
+const GET_ALL_JOBS_QUERY = gql`
+  query GetAllJobs($keyword: String) {
+    getAllJobs(keyword: $keyword) {
+      id
+      title
+      description
+      requirements
+      salary
+      experienceLevel
+      location
+      jobType
+      position
+      company {
+        name
+        logo
+      }
+      created_by {
+        fullName
+      }
+
+      createdAt
+      updatedAt
+    }
+  }
+`;
 const useGetAllJobs = () => {
+  const { loading, error, data, refetch } = useQuery(GET_ALL_JOBS_QUERY);
+  console.log("data", data);
   // console.log("hello")
   const dispatch = useDispatch();
   const { searchedQuery } = useSelector((store) => store.job);
   useEffect(() => {
-    const fetchAllJobs = async () => {
-      try {
-        const res = await axios.get(
-          // `${JOB_API_END_POINT}/get?keyword=${searchedQuery}`,
-          `${JOB_API_END_POINT}/get`,
-          { withCredentials: true }
-        );
-        if (res.data.success) {
-          dispatch(setAllJobs(res.data.jobs));
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchAllJobs();
-  }, []);
+    if (data && data.getAllJobs) {
+      console.log("data and jobs client by grapql", data.getAllJobs);
+      dispatch(setAllJobs(data.getAllJobs));
+    }
+
+    if (loading) {
+      console.log("Fetching jobs...");
+    }
+    if (error) {
+      console.error("Error fetching jobs:", error.message);
+    }
+  }, [data, loading, error, dispatch]);
 };
 
 export default useGetAllJobs;
