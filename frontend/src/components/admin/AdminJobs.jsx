c5e1a023b300af54243e21b728dda5aa8f19c983
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-17 23:11:02 +0600

    feat: sent adminId to use-get-all-admin-jobs hook

--- frontend/src/components/admin/AdminJobs.jsx
+++ frontend/src/components/admin/AdminJobs.jsx
@@ -3,14 +3,17 @@ import Navbar from "../shared/Navbar";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import AdminJobsTable from "./AdminJobsTable";
 import useGetAllAdminJobs from "@/hooks/useGetAllAdminJobs";
 import { setSearchJobByText } from "@/redux/jobSlice";
 import Navbar_two from "../shared/Navbar_two";
+import store from "@/redux/store";
 
 const AdminJobs = () => {
-  useGetAllAdminJobs();
+  const { user } = useSelector((store) => store.auth);
+  const adminId = user?._id
+  useGetAllAdminJobs(adminId);
   const [input, setInput] = useState("");
   const navigate = useNavigate();
   const dispatch = useDispatch();
