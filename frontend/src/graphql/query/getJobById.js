59e344e70097c826e0f72a5dfa58626d59b4ba84
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-17 23:06:11 +0600

    feat: add graphql query to bring single job

---
+++ frontend/src/graphql/query/getJobById.js
@@ -0,0 +1,38 @@
+import { gql } from "@apollo/client";
+
+export const GET_JOB_bY_ID = gql`
+  query GetJobById($jobId: ID!) {
+    getJobById(jobId: $jobId) {
+      id
+      title
+      description
+      requirements
+      salary
+      experienceLevel
+      location
+      jobType
+      position
+      company {
+        id
+        name
+        description
+        website
+        location
+        logo
+        userId
+        createdAt
+        updatedAt
+      }
+      created_by {
+        fullName
+      }
+      applications {
+        applicant {
+          fullName
+        }
+      }
+      createdAt
+      updatedAt
+    }
+  }
+`;
