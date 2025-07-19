e6c7e42ee6e7ea6a2c1c10a15130582a1db511e1
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-19 21:20:22 +0600

    feat: add applo-client

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,16 +7,24 @@ import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import { persistStore } from "redux-persist";
 import { PersistGate } from "redux-persist/integration/react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+  cache: new InMemoryCache(),
+});
 
 const persistor = persistStore(store);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <App />
-        <Toaster />
-      </PersistGate>
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+          <Toaster />
+        </PersistGate>
+      </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
