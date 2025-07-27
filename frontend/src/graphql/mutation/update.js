66a58c884f345a882de03f90696626747300c91d
“farah-jannat” <farahjannatmithila@gmail.com>
2025-07-27 17:48:19 +0600

    refactor: update updateuser

--- frontend/src/graphql/mutation/update.js
+++ frontend/src/graphql/mutation/update.js
@@ -1,8 +1,29 @@
 import { gql } from "@apollo/client";
 
 export const UPDATE_USER = gql`
-  mutation Update($updateInput: updateInput) {
-    update(updateInput: $updateInput) {
+  mutation Update(
+    $fullName: String
+    $userId: ID!
+    $email: String
+    $bio: String
+    $phoneNumber: String
+    # password: String
+    $role: String
+    $profilePhoto: String
+    $resume: String # resumeOriginalName: String
+  ) {
+    update(
+      updateInput: {
+        fullName: $fullName
+        userId: $userId
+        email: $email
+        bio: $bio
+        phoneNumber: $phoneNumber
+        role: $role
+        profilePhoto: $profilePhoto
+        resume: $resume
+      }
+    ) {
       id
       fullName
       email
