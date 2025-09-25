e603cf791c03cf748cd641ccfcb25cb3b82cb0bf
“farah-jannat” <farahjannatmithila@gmail.com>
2025-09-25 18:53:17 +0600

    style: change footer style

--- frontend/src/components/shared/Footer.jsx
+++ frontend/src/components/shared/Footer.jsx
@@ -1,46 +1,87 @@
+import {
+  Facebook,
+  Github,
+  Instagram,
+  InstagramIcon,
+  Twitter,
+} from "lucide-react";
+import { Separator } from "@radix-ui/react-select";
 import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="border-t border-t-gray-200 py-8">
-      <div className="container mx-auto  px-4">
-        <div className="flex flex-col md:flex-row justify-between items-center">
-          <div className="mb-4  md:mb-0">
-            <h2 className="text-xl font-bold">Job Hunt</h2>
-            <p className="text-sm">@ 2025 Your Company. All rights reserved</p>
+    <div className="card-gradient-brown px-[60px] flex flex-col items-startl gap-[15px] pt-[47px] pb-[15px]">
+      <div className=" flex items-start justify-between gap-[50px] w-full">
+        <div className="grid gap-[35px] ">
+          <div>
+            <h1 className="text-[18px]  ">
+              <span className="text-[#287992]">Niyog</span>
+              <span className="text-[#E8C092]">Zone</span>
+            </h1>
           </div>
-          <div className="flex space-x-4 md:mt-0 ">
-            <a
-              href="https://facebook.com"
-              className="hover:text-gray-400"
-              aria-label="Facebook"
-            >
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M22.676 0H1.324C.593 0 0 .592 0 1.324v21.352C0 23.408.593 24 1.324 24H12.82V14.706H9.692v-3.578h3.128V8.408c0-3.1 1.893-4.787 4.657-4.787 1.325 0 2.463.1 2.794.144v3.238l-1.918.001c-1.503 0-1.794.715-1.794 1.762v2.31h3.587l-.468 3.578h-3.119V24h6.116C23.407 24 24 23.408 24 22.676V1.324C24 .592 23.407 0 22.676 0z" />
-              </svg>
-            </a>
-            <a
-              href="https://twitter.com"
-              className="hover:text-gray-400"
-              aria-label="Twitter"
-            >
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M24 4.557a9.835 9.835 0 01-2.828.775 4.934 4.934 0 002.165-2.724 9.867 9.867 0 01-3.127 1.195 4.924 4.924 0 00-8.38 4.49A13.978 13.978 0 011.67 3.149 4.93 4.93 0 003.16 9.724a4.903 4.903 0 01-2.229-.616v.062a4.93 4.93 0 003.946 4.827 4.902 4.902 0 01-2.224.084 4.93 4.93 0 004.6 3.417A9.869 9.869 0 010 21.543a13.978 13.978 0 007.548 2.212c9.057 0 14.01-7.507 14.01-14.01 0-.213-.004-.425-.015-.636A10.012 10.012 0 0024 4.557z" />
-              </svg>
-            </a>
-            <a
-              href="https://linkedin.com"
-              className="hover:text-gray-400"
-              aria-label="LinkedIn"
-            >
-              <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 24 24">
-                <path d="M20.447 20.452H16.85v-5.569c0-1.327-.027-3.037-1.852-3.037-1.854 0-2.137 1.446-2.137 2.94v5.666H9.147V9.756h3.448v1.464h.05c.48-.91 1.653-1.871 3.401-1.871 3.634 0 4.307 2.39 4.307 5.498v5.605zM5.337 8.29c-1.105 0-2-.896-2-2 0-1.106.895-2 2-2 1.104 0 2 .895 2 2 0 1.104-.896 2-2 2zM7.119 20.452H3.553V9.756h3.566v10.696zM22.225 0H1.771C.791 0 0 .774 0 1.729v20.542C0 23.226.792 24 1.771 24h20.451c.979 0 1.771-.774 1.771-1.729V1.729C24 .774 23.205 0 22.225 0z" />
-              </svg>
-            </a>
+          <p className="text-[14px] text-[#393939] w-[228px]">
+            We have clothes that suits your style and which you’re proud to
+            wear. From women to men.
+          </p>
+          <div className="flex items-center gap-[12px]">
+            <div className="w-[24px] h-[24px] grid border border-[#6392D8]  place-items-center rounded-full bg-[#FFFFFF]">
+              <Twitter size={14} color="#6392D8" />
+            </div>
+            <div className="w-[24px] h-[24px] grid place-items-center rounded-full bg-[#6392D8]">
+              <Facebook size={14} color="#FFFFFF" />
+            </div>
+            <div className="w-[24px] h-[24px] grid border border-[#6392D8]  place-items-center rounded-full bg-[#FFFFFF]">
+              <Instagram size={14} color="#6392D8" />
+            </div>
+            <div className="w-[24px] h-[24px] grid border border-[#6392D8] place-items-center rounded-full bg-[#FFFFFF]">
+              <Github size={14} color="#6392D8" />
+            </div>
           </div>
         </div>
+
+        <div className=" text-[14px] text-[#35373F] hidden sm:grid gap-[13px]">
+          <h2 className="text-[#03050F] font-bold">COMPANY</h2>
+          <span>Features</span>
+          <span>Works</span>
+          <span>Carear</span>
+          <span>About</span>
+        </div>
+        <div className=" text-[14px] text-[#35373F] hidden sm:grid gap-[13px]">
+          <h2 className="text-[#03050F] font-bold">HELP</h2>
+          <span>Customer Support</span>
+          <span>Delivery Details</span>
+          <span>Terms & Conditions</span>
+          <span>Privary Policy</span>
+        </div>
+        <div className=" text-[14px] text-[#35373F] hidden md:grid gap-[13px]">
+          <h2 className="text-[#03050F] font-bold">FAQ</h2>
+          <span>Account</span>
+          <span>Manage Deliveries</span>
+          <span>Orders</span>
+          <span>Payments</span>
+        </div>
+        <div className=" text-[14px] text-[#35373F] hidden lg:grid gap-[13px]">
+          <h2 className="text-[#03050F] font-bold">COMPANY</h2>
+          <span>Features</span>
+          <span>Works</span>
+          <span>Carear</span>
+          <span>About</span>
+        </div>
       </div>
-    </footer>
+      <Separator className="border-b" />
+      <div className="flex items-center justify-between ">
+        <p className="text-[12px] text-[#393939]">
+          Kajbazar @ 2025, All Rights Reserved
+        </p>
+        <div className="hidden sm:flex items-center gap-[8px]">
+          {[1, 2, 3, 4, 5].map((item) => (
+            <div className="w-[24px] h-[24px] grid border border-[#6392D8]  place-items-center rounded-full bg-[#FFFFFF]">
+              <Twitter size={14} color="#6392D8" />
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 };
 
