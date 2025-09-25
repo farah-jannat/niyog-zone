dbda78b3cc540c395b054d6cc2b561f0e78df17e
“farah-jannat” <farahjannatmithila@gmail.com>
2025-09-25 18:55:00 +0600

    Update latest jobs design

--- frontend/src/components/LatestJobs.jsx
+++ frontend/src/components/LatestJobs.jsx
@@ -10,19 +10,42 @@ const LatestJobs = () => {
   console.log("all job here", allJobs);
   const navigate = useNavigate();
   return (
-    <div className="w-[90%] lg:w-[80%] mx-auto my-10">
-      <div className="flex flex-col items-center gap-4">
-        <h2 className="text-2xl font-semibold text-center">Latest Jobs Post</h2>
-        <p className="text-gray-500 text-center">
-          Find the job thats prfect for you, about 800+ new jobs everyday
-        </p>
-        <div className="flex items-center justify-center flex-wrap gap-3 sm:gap-4">
-          {/* <div className=" flex items-center gap-2 border border-Blue rounded-md px-3 py-2">
+    <div className="mt-[68px]">
+      <div className="max-h-[76px] my-[68px] flex items-center justify-between">
+        <div>
+          <p className="text-[#3E3F47] text-[12px]">
+            Top Jobs You Can Apply Right Now
+          </p>
+          <h2 className="text-[32px] text-[#0E0F19] font-medium">
+            Latest Jobs
+          </h2>
+        </div>
+       
+        <button className=" bg-[#E8C092] text-[#03050F]  w-[89px] h-[33px]  rounded-[4px] text-[14px]">
+          Explore
+        </button> 
+      </div>
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-5"> */}
+        <div className="grid md:grid-cols-2 xl:grid-cols-3 gap-[16px]">
+          {allJobs?.length <= 0 ? (
+            <span>No Job Available</span>
+          ) : (
+            allJobs?.slice(0, 6).map((job) => (
+              <div>
+                {" "}
+                <LatestJobsCards key={job.id} job={job} />
+              </div>
+            ))
+          )}
+        </div>
+      {/* </div> */}
+      {/* <div className="flex items-center justify-center flex-wrap gap-3 sm:gap-4">
+          <div className=" flex items-center gap-2 border border-Blue rounded-md px-3 py-2">
             <Home size={20} className="text-Blue" />
             <button className="text-sm font-semibold text-Blue ">
               Marketing
             </button>
-          </div> */}
+          </div>
           {["Marketing", "Software", "UI/UX", "Develop", "Mid", "Senior"].map(
             (item, idx) => (
               <div
@@ -31,15 +54,15 @@ const LatestJobs = () => {
                   navigate(`/jobs?search=${item}`);
                 }}
               >
-                {/* <Home size={22} strokeWidth="1" className="text-blue-900" /> */}
+                <Home size={22} strokeWidth="1" className="text-blue-900" />
 
                 {item}
               </div>
             )
           )}
         </div>
-      </div>
-      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-5"> */}
+      
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 my-5">
       <div className="flex items-center justify-center  gap-3 flex-wrap ">
         {allJobs?.length <= 0 ? (
           <span>No Job Available</span>
@@ -52,6 +75,7 @@ const LatestJobs = () => {
           ))
         )}
       </div>
+    </div> */}
     </div>
   );
 };
