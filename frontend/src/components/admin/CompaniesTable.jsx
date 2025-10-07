f8e6d9cbd48b1a4cb9df42594051eb7aaea68edb
“farah-jannat” <farahjannatmithila@gmail.com>
2025-10-07 13:35:50 +0600

    update company-table design

--- frontend/src/components/admin/CompaniesTable.jsx
+++ frontend/src/components/admin/CompaniesTable.jsx
@@ -10,7 +10,7 @@ import {
 } from "../ui/table";
 import { Avatar, AvatarImage } from "../ui/avatar";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
-import { Edit2, MoreHorizontal } from "lucide-react";
+import { Delete, Edit2, MoreHorizontal } from "lucide-react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 
@@ -21,7 +21,7 @@ const CompaniesTable = () => {
   );
   const [filterCompany, setFilterCompany] = useState(companies);
 
-  //   console.log("here is all company admin created", companies);
+  console.log("here is all company admin created", companies);
   useEffect(() => {
     const filteredCompany =
       companies.length >= 0 &&
@@ -36,44 +36,72 @@ const CompaniesTable = () => {
     setFilterCompany(filteredCompany);
   }, [companies, searchCompanyByText]);
   return (
-    <div>
+    <div className="bg-white rounded-[8px] border border-[#E6E6E7] ">
       <Table>
         <TableCaption>A list of your recent registered companies</TableCaption>
         <TableHeader>
-          <TableRow>
-            <TableHead>Logo</TableHead>
-            <TableHead>Name</TableHead>
-            <TableHead>Date</TableHead>
-            <TableHead className="text-right">Action</TableHead>
+          <TableRow className="bg-[#287992] hover:bg-[#287992] rounded-[8px]">
+            <TableHead className="text-[#F7F7FA] text-[12px] font-semibold">
+              Logo
+            </TableHead>
+            <TableHead className="text-[#F7F7FA] text-[12px] font-semibold">
+              Name
+            </TableHead>
+            <TableHead className="text-[#F7F7FA] text-[12px] font-semibold">
+              Date
+            </TableHead>
+            <TableHead className="text-right text-[#F7F7FA] text-[12px] font-semibold'">
+              Action
+            </TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {filterCompany?.map((company) => (
-            <tr >
+            // <tr>
+            <TableRow>
               <TableCell>
-                <Avatar>
-                  <AvatarImage src={company.logo} />
-                </Avatar>
+                {/* <Avatar>
+                    <AvatarImage src={company.logo} />
+                  </Avatar> */}
+                <div className="h-[37px] w-[79px]">
+                  <img
+                    src={company.logo}
+                    alt=""
+                    className="w-full h-full rounded-[4px] object-cover"
+                  />
+                </div>
               </TableCell>
               <TableCell>{company.name}</TableCell>
               <TableCell>{company.createdAt.split("T")[0]}</TableCell>
-              <TableCell className="text-right cursor-pointer">
-                <Popover>
-                  <PopoverTrigger>
-                    <MoreHorizontal />
-                  </PopoverTrigger>
-                  <PopoverContent
-                    className="w-32 hover:bg-gray-200"
+              <TableCell className="">
+                <div className="flex items-center justify-end gap-[18px]">
+                  <Delete size={22} className="text-red-600 cursor-pointer" />
+
+                  <Edit2
+                    size={18}
+                    className="cursor-pointer"
                     onClick={() => navigate(`/admin/companies/${company.id}`)}
-                  >
-                    <div className="flex items-center gap-2 w-fit cursor-pointer">
-                      <Edit2 className="w-4" />
-                      <span>Edit</span>
-                    </div>
-                  </PopoverContent>
-                </Popover>
+                  />
+                </div>
               </TableCell>
-            </tr>
+              {/* <TableCell className="text-right cursor-pointer">
+                  <Popover>
+                    <PopoverTrigger>
+                      <MoreHorizontal />
+                    </PopoverTrigger>
+                    <PopoverContent
+                      className="w-32 hover:bg-gray-200"
+                      onClick={() => navigate(`/admin/companies/${company.id}`)}
+                    >
+                      <div className="flex items-center gap-2 w-fit cursor-pointer">
+                        <Edit2 className="w-4" />
+                        <span>Edit</span>
+                      </div>
+                    </PopoverContent>
+                  </Popover>
+                </TableCell> */}
+            </TableRow>
+            // </tr>
           ))}
         </TableBody>
       </Table>
