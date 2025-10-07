8c53022137c45d76cb0eb971eeecd4bf62093cbf
“farah-jannat” <farahjannatmithila@gmail.com>
2025-10-07 13:41:53 +0600

    add container

--- frontend/src/components/container.jsx
+++ frontend/src/components/container.jsx
@@ -5,7 +5,7 @@ const Container = ({ children, className }) => {
     <div className={`${className}`}>
       <div
         className={`px-[16px] max-w-[450px] md:max-w-[1000px] mx-auto  md:px-[24px] xl:px-[60px] xl:max-w-[1440px] 
-          ${className} `}
+          `}
       >
         {children}
       </div>
