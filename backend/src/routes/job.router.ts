f884626582213d3ec8490a6626cc07b5b698fcd0
asifulhaque087 <asifulhaque087@gmail.com>
2025-10-28 16:55:41 +0600

    feat: add latest job api

--- backend/src/routes/job.router.ts
+++ backend/src/routes/job.router.ts
@@ -1,16 +1,12 @@
 import { Router } from "express";
-import {
-  getAdminJobs,
-  getAllJobs,
-  getJobById,
-  postJob,
-} from "@/controllers/job.controller";
+import { getLatestJobs } from "@/controllers/job.controller";
 import isAuthenticated from "@/middlewares/isAuthenticated";
 
 const jobRouter = Router();
 
 // jobRouter.post("/jobs", isAuthenticated, postJob);
-// jobRouter.get("/jobs", getAllJobs);
+// jobRouter.get("/", getAllJobs);
+jobRouter.get("/latest", getLatestJobs);
 // jobRouter.get("/jobs/:id", getJobById);
 // jobRouter.get("/jobs/admin/:id", isAuthenticated, getAdminJobs);
 
