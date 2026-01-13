4638a04072c8fa856b7c363ca9a312d2c65211d1
“farah-jannat” <farahjannatmithila@gmail.com>
2026-01-13 12:06:16 +0600

    feat: add job and company edit api

--- frontend/src/features/company/api/mutations.api.ts
+++ frontend/src/features/company/api/mutations.api.ts
@@ -1,6 +1,7 @@
 import { company__axios } from "@/axios.service";
 import { Company } from "@/features/company/schemas/company.schema";
 import { InsertCompanyType } from "@/features/company/schemas/create-company.schema";
+import { EditCompanyType } from "@/features/company/schemas/edit-company.schema";
 
 export const createCompany = async (data: InsertCompanyType) => {
   const response = await company__axios.post<Company>(`/`, data);
@@ -9,5 +10,12 @@ export const createCompany = async (data: InsertCompanyType) => {
 
 export const deleteCompany = async(id:string)=>{
   const response = await company__axios.delete<Company>(`/${id}`)
+  return response.data
+}
+
+
+export const editCompany = async(data:EditCompanyType)=>{
+  console.log("data for updating company", data)
+  const response = await company__axios.put<Company>(`/`, data)
   return response.data
 }
--- frontend/src/features/job/api/mutations.api.ts
+++ frontend/src/features/job/api/mutations.api.ts
@@ -1,8 +1,17 @@
 import { job__axios } from "@/axios.service";
 import { InsertJobType } from "@/features/job/schemas/create-job.schema";
 import { Job } from "@/features/job/schemas/job.schema";
+import { UpdateJobType } from "@/features/job/schemas/update-job.schema";
 
 export const createJob = async (data: InsertJobType) => {
   const response = await job__axios.post<Job>(`/`, data);
   return response.data;
 };
+
+
+export const updateJob = async (data:UpdateJobType)=>{
+  console.log("data for updting job from job mutations api - ", data)
+  const response = await job__axios.put<Job>(`/`, data)
+  return response.data;
+}
+
